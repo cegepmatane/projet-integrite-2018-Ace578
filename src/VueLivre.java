b479a466cc9d175c433cc1117fe19a84ff580c50
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-09-04 21:23:55 -0400

    Création d'un premier livre dans la VueLivre

--- src/VueLivre.java
+++ src/VueLivre.java
@@ -1,15 +1,47 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class VueLivre extends Application {
+	
+	
+	protected Label valeurTitre;
+	protected Label idTitre;
+	protected Label  valeurAnnee;
+	protected Label idAnnee;
+	protected Label valeurStyle;
+	protected Label idStyle;
+	
+
 
 
 	@Override
 	public void start(Stage stade) throws Exception {
-		Pane racine = new Pane();
-		stade.setScene(new Scene(racine, 300, 250));
+		
+		
+		GridPane grilleLivre = new GridPane();
+		
+		idTitre = new Label("Titre : ");
+		valeurTitre = new Label("Le Seigneur des Anneaux");		
+		grilleLivre.add(idTitre, 0, 0);
+		grilleLivre.add(valeurTitre, 1, 0);
+		
+		idAnnee = new Label("Annee : ");
+		valeurAnnee = new Label("1954");
+		grilleLivre.add(valeurAnnee, 1, 1);
+		grilleLivre.add(idAnnee, 0, 1);
+		
+		idStyle = new Label("Style : ");
+		valeurStyle = new Label("Fantasy");
+		grilleLivre.add(idStyle, 0, 3);
+		grilleLivre.add(valeurStyle, 1, 3);
+		
+		
+		stade.setScene(new Scene(grilleLivre, 300, 250));
 		stade.show();
+		
 	}
 }
