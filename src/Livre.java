84404584ccb5eb8299866b2c66c04e242cf62fe7
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-09-04 21:29:10 -0400

    Création d'une classe "Livre"

---
+++ src/Livre.java
@@ -0,0 +1,66 @@
+
+public class Livre {
+	
+	protected String titre;
+	protected String annee;
+	protected String style;
+	
+	
+	public Livre(String titre) {
+		super();
+		this.titre = titre;
+	}
+
+
+	public Livre(String titre, String annee) {
+		super();
+		this.titre = titre;
+		this.annee = annee;
+	}
+
+
+	public Livre(String titre, String annee, String style) {
+		super();
+		this.titre = titre;
+		this.annee = annee;
+		this.style = style;
+	}
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+
+	public String getAnnee() {
+		return annee;
+	}
+
+
+	public void setAnnee(String annee) {
+		this.annee = annee;
+	}
+
+
+	public String getStyle() {
+		return style;
+	}
+
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
