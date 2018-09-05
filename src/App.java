3de7906d86cf9a55cfd97f289e5945718d35b03b
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-09-04 21:05:09 -0400

    Création d'App et lancement de l'application pour tester

---
+++ src/App.java
@@ -0,0 +1,9 @@
+
+public class App {
+
+	public static void main(String[] parametres) {
+		VueLivre.launch(VueLivre.class,parametres);
+
+	}
+
+}
--- src/VueLivre.java
+++ src/VueLivre.java
@@ -1,4 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class VueLivre {
+public class VueLivre extends Application {
 
+
+	@Override
+	public void start(Stage stade) throws Exception {
+		Pane racine = new Pane();
+		stade.setScene(new Scene(racine, 300, 250));
+		stade.show();
+	}
 }
