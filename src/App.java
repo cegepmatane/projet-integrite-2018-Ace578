148c09c7171a9caec2996032d4c5803e43f32435
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-09-13 17:25:20 -0400

    création de la vue d'édition d'un prix et test

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import vue.NavigateurDesVues;
 import vue.VueAjouterLivre;
 import vue.VueAjouterPrix;
+import vue.VueEditerPrix;
 import vue.VueListeLivre;
 import vue.VueListePrix;
 import vue.VueLivre;
@@ -9,7 +10,7 @@ public class App {
 
 	public static void main(String[] parametres) {
 		//NavigateurDesVues.launch(NavigateurDesVues.class,parametres);
-		VueAjouterPrix.launch(VueAjouterPrix.class, parametres);
+		VueEditerPrix.launch(VueEditerPrix.class, parametres);
 
 	}
 
--- src/vue/VueEditerPrix.java
+++ src/vue/VueEditerPrix.java
@@ -1,25 +1,30 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import modele.Prix;
-public class VueAjouterPrix extends Application{
+public class VueEditerPrix extends Application {
 	protected TextField valeurNom;
 	protected TextField valeurPromotion;
 	protected TextField valeurDescription;
 	
-	
 	@Override
 	public void start(Stage stade) throws Exception {
+		
+		
 		VBox panneau = new VBox();	
 		GridPane grillePrix = new GridPane();
+		Button actionEnregistrerPrix = new Button("Enregistrer");
+		
+		
 		
 		valeurNom = new TextField();
 		grillePrix.add(new Label("Nom : "), 0, 0);
@@ -31,21 +36,27 @@ public class VueAjouterPrix extends Application{
 		valeurDescription = new TextField("");
 		grillePrix.add(new Label("Description : "), 0, 2);
 		grillePrix.add(valeurDescription, 1, 2);		
+		
 						
 			
 		
-		panneau.getChildren().add(new Label("Ajouter un Prix")); 
+		panneau.getChildren().add(new Label("Editer un Prix")); 
 		panneau.getChildren().add(grillePrix);
-		panneau.getChildren().add(new Button("Enregistrer"));
+		panneau.getChildren().add(actionEnregistrerPrix);
 		stade.setScene(new Scene(panneau, 400, 400));
-		stade.show();		
+		stade.show();
 	}
 	
 	public Prix demanderPrix()
 	{
 		Prix Prix = new Prix(valeurNom.getText(), 
 								valeurPromotion.getText()); 
-				
+								
 		return Prix;
 	}
+
+	
+	
+	
+	
 }
